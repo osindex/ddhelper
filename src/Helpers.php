94d06b9cf2b7951558417aed9b14bafb218f60c9
osi <yaoiluo@gmail.com>
2018-04-08 17:32:07 +0800

    remove other packages in func

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,70 +1,4 @@
 <?php
-if (!function_exists('app')) {
-	/**
-	 * 获取Di容器
-	 *
-	 * @param  string  $abstract
-	 * @return mixed|EasySwoole\Core\Component\Di
-	 */
-	function app($abstract = null) {
-		if (is_null($abstract)) {
-			return EasySwoole\Core\Component\Di::getInstance();
-		}
-		return EasySwoole\Core\Component\Di::getInstance()->get($abstract);
-	}
-}
-if (!function_exists('config')) {
-	/**
-	 * Generate a form field to spoof the HTTP verb used by forms.
-	 *
-	 * @param  string  $method
-	 * @return \Illuminate\Support\HtmlString
-	 */
-	function config($param = null) {
-		return \EasySwoole\Config::getInstance()->getConf($param);
-	}
-}
-if (!function_exists('url')) {
-	/**
-	 * 转发静态文件
-	 *      config('ForwardingDomain')  转发域名
-	 * @param string $path 静态文件路径
-	 * @return string
-	 */
-	function url($path = '') {
-		return config('app.url') . '/' . $path;
-	}
-}
-if (!function_exists('user')) {
-	/**
-	 * Generate a form field to spoof the HTTP verb used by forms.
-	 *
-	 * @param  string  $method
-	 * @return \Illuminate\Support\HtmlString
-	 */
-	function user($header) {
-		$jwtConf = config('jwt');
-		$tokenBase = head($header);
-		$token = str_replace($jwtConf['bearer'], '', $tokenBase);
-		$info = jwtdecode($token);
-		if (!$info) {
-			return null;
-		} else {
-			return getUser($info['sub']);
-		}
-	}
-}
-if (!function_exists('collect')) {
-	/**
-	 * Create a collection from the given value.
-	 *
-	 * @param  mixed  $value
-	 * @return \Illuminate\Support\Collection
-	 */
-	function collect($value = null) {
-		return new \Illuminate\Support\Collection($value);
-	}
-}
 if (!function_exists('now')) {
 	/**
 	 * Create a collection from the given value.
@@ -75,9 +9,6 @@ if (!function_exists('now')) {
 	function now($value = null) {
 		return date('Y-m-d H:i:s');
 	}
-}
-function ossUrl($expend = '') {
-	return config('oss.url', 'https://statics.ddchuansong.com/') . $expend;
 }
 function bfb($val, $dot) {
 	$str = round($val, $dot + 2);
@@ -139,39 +70,6 @@ function getNeedBetween($kwd, $mark1, $mark2 = '') {
 	$kw = substr($kw, $startnum, $endnum);
 	return $kw;
 }
-
-function jwtencode($id) {
-	$time = time();
-	$jwtconf = config('jwt');
-	$token = array(
-		"exp" => $time + $jwtconf['exp'],
-		"iat" => $time + $jwtconf['iat'],
-		"nbf" => $time + $jwtconf['nbf'],
-		"sub" => $id,
-	);
-	$token = \Firebase\JWT\JWT::encode($token, $jwtconf['key']);
-	return $token;
-}
-function jwtdecode(String $token, $force = false) {
-	$jwtconf = config('jwt');
-	try {
-		$info = \Firebase\JWT\JWT::decode($token, $jwtconf['key'], array('HS256'));
-		// var_dump($info);
-		$decoded_array = (array) $info;
-		return $decoded_array;
-	} catch (\Firebase\JWT\ExpiredException $e) {
-		// at this point the jwt was validated ok
-		if ($force) {
-			list($headb64, $bodyb64, $cryptob64) = explode('.', $token);
-			$payload = base64_decode($bodyb64);
-			return $payload;
-		} else {
-			return 'Token已过期!';
-		}
-	} catch (\Exception $e) {
-		return 'Token已过期!';
-	}
-}
 //将XML转为array
 function xmlToArray($xml) {
 	//禁止引用外部xml实体
@@ -182,44 +80,12 @@ function xmlToArray($xml) {
 function base64EncodeImageDate($image_data) {
 	return $base64_image = 'data:image/png;base64,' . chunk_split(base64_encode($image_data));
 }
-/**
- * [makeQrcode description]
- * @param  [type] $options 文本信息,宽高,false图片base64信息
- * @return [type]          [description]
- */
-function makeQrcode(...$options) {
-	// var_dump($options);
-	$url = $options[0];
-	$size = $options[1] ?? 250;
-	$steam = $options[2] ?? false;
-	$qrSteam = new Endroid\QrCode\QrCode($url);
-	$qrSteam->setSize($size);
-	if ($steam) {
-		header('Content-Type: ' . $qrSteam->getContentType());
-		return $qrSteam->writeString();
-	} else {
-		$tmpname = md5($url);
-		$tempfile = tempnam('tmp', $tmpname);
-		$qrSteam->writeFile($tempfile);
-		return base64EncodeImage($tempfile);
-		@unlink($tempfile);
-	}
-}
 function base64EncodeImage($image_file) {
 	$base64_image = '';
 	$image_info = getimagesize($image_file);
 	$image_data = fread(fopen($image_file, 'r'), filesize($image_file));
 	$base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
 	return $base64_image;
-}
-function userExpressPermission($state = 0) {
-	$permission = 0;
-	if (in_array((int) $state, [0, 1])) {
-		$permission = 7;
-	} else {
-		$permission = 4;
-	}
-	return $permission;
 }
 function array_only_null(array $array, array $keys, $default = null) {
 	$return = [];
